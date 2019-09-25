ac9a8fad948808d1cf0fa5e3c2c21d8314e975ec
Dion Hulse <contact@dd32.id.au>
2019-09-25 10:44:53 +0100

    Block Directory: REST API: Use the plugins name when the specific block name is unavailable.

--- lib/class-wp-rest-block-directory-controller.php
+++ lib/class-wp-rest-block-directory-controller.php
@@ -352,10 +352,7 @@ class WP_REST_Block_Directory_Controller extends WP_REST_Controller {
 		// There might be multiple blocks in a plugin. Only the first block is mapped.
 		$block_data   = reset( $plugin['blocks'] );
 		$block->name  = $block_data['name'];
-		$block->title = $block_data['title'];
-		if ( ! $block->title ) {
-			$block->title = $plugin['name'];
-		}
+		$block->title = $block_data['title'] ?: $plugin['name'];
 
 		// Plugin's description, not description in block.json.
 		$block->description = wp_trim_words( wp_strip_all_tags( $plugin['description'] ), 30, '...' );
